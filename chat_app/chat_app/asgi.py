c564b1b1a132db68141aa3ea408e9136ff8a9f11
Dan-njuguna <njugush020@gmail.com>
2024-03-05 20:37:43 +0300

    chat room...django back-end

---
+++ chat_app/chat_app/asgi.py
@@ -0,0 +1,27 @@
+"""
+ASGI config for chat_app project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'chat_app.settings')
+
+application = get_asgi_application()
+
+ASGI_APPLICATION = "chat_app.asgi.application"
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            'hosts': [('127.0.0.1', 6379)],
+        },
+    },
+}
