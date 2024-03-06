34c9047e0dfba4bfb55cc33a9ed9e1e0062968cd
Dan-njuguna <njugush020@gmail.com>
2024-03-06 22:51:51 +0300

    chat app update

--- chat_app/chat/views.py
+++ chat_app/chat/views.py
@@ -3,3 +3,17 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'chat/index.html')
+
+def room(request, room_name):
+    username = request.GET.get('username', 'Anonymous')
+    
+    return render(request, 'chat/room.html', {'room_name': room_name, 'username': username})
+
+# Import the messages model
+from .models import Message
+
+# Get the messages from the database
+messages = Message.objects.filter(room=room_name)[0:25]
+
+# Add the messages to the context
+'messages': messages
