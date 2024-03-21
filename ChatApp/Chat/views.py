e58b2efe1531cfc5454a3684f70a7e5c1ac2c0a8
Dan-njuguna <njugush020@gmail.com>
2024-03-21 15:02:08 +0300

    views.py

--- ChatApp/Chat/views.py
+++ ChatApp/Chat/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from .form import Form
 
 class LoginView(APIView):
     def get(self, request):
