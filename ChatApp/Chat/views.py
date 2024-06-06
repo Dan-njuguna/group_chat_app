ddb82125d5e829f540bed39fd3cd0b8706a1260a
Dan-njuguna <njugush020@gmail.com>
2024-06-06 18:47:58 +0300

    views.py changes in path

--- ChatApp/Chat/views.py
+++ ChatApp/Chat/views.py
@@ -5,10 +5,10 @@ from .models import Room, Message
 @login_required
 def index(request):
     rooms = Room.objects.all()
-    return render(request, 'chat/index.html', {'rooms': rooms})
+    return render(request, 'home.html', {'rooms': rooms})
 
 @login_required
 def room(request, room_name):
     room = get_object_or_404(Room, name=room_name)
     messages = room.messages.order_by('timestamp')
-    return render(request, 'chat/room.html', {'room': room, 'messages': messages})
+    return render(request, 'room.html', {'room': room, 'messages': messages})
