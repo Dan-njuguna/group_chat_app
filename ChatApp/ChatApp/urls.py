f161f54f2a1f0a4fcfd1ed4cf2efa2d93300d9f7
Dan-njuguna <njugush020@gmail.com>
2024-06-18 09:05:15 +0300

    Deletions: All files

--- ChatApp/ChatApp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('chat/', include('Chat.urls')),  # Include your chat app's URLs
-    path('accounts/', include('django.contrib.auth.urls')),  # Include the authentication URLs
-]
